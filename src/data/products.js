a17101fd6faa73dd64c0e7a1faffa49a7921fd4b
Florencia Valladares <fvalladares1@uc.cl>
2019-05-14 11:24:34 -0400

    add products data

---
+++ src/data/products.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    name: 'MacBook Pro Touchbar 2017',
+    details: '8 GB ram, 256 GB SSD',
+    price: 750000,
+    category: ['electronics', 'apple', 'macbook'],
+    stars: 5,
+  },
+  {
+    name: 'MacBook Pro Touchbar 2016',
+    details: '16 GB ram, 500 GB SSD',
+    price: 450000,
+    category: ['electronics', 'apple', 'macbook'],
+    stars: 4,
+  },
+  {
+    name: 'Notebook 15.6" AMD A9 12GB 1TB|15-db0005la',
+    details: `Modelo 15-db0005la. 
+    Procesador AMD A9. 
+    Velocidad del procesador de 3.1 GHz. 
+    Memoria RAM de 12GB.
+    Disco duro de 1TB.
+    Tamaño de la pantalla 15.6.`,
+    price: 550000,
+    category: ['electronics', 'asus', 'laptop'],
+    stars: 5,
+  },
+  {
+    name: 'Notebook 14" Intel Ci5 8GB 1TB|14-ce0001la',
+    details: '8 GB ram, 256 GB SSD',
+    price: 750800,
+    category: ['electronics', 'hp', 'laptop'],
+    stars: 3,
+  },
+  {
+    name: 'MacBook Pro Touchbar 2019',
+    details: '8 GB ram, 256 GB SSD',
+    price: 1750000,
+    category: ['electronics', 'apple', 'macbook'],
+    stars: 5,
+  },
+];
