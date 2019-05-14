f3872fecf12dc7d00e20d2cb37227ec50b5e2679
Florencia Valladares <fvalladares1@uc.cl>
2019-05-14 13:23:58 -0400

    change image url

--- src/data/products.js
+++ src/data/products.js
@@ -4,7 +4,7 @@ export default [
     details: '8 GB ram, 256 GB SSD',
     price: 750000,
     category: ['electronics', 'apple', 'macbook'],
-    imgUrl: 'https://zdnet3.cbsistatic.com/hub/i/r/2018/08/23/ee30e744-889f-4df4-86db-cdac30453d08/thumbnail/770x433/2c2d7c9bb4b6131a1782b720a86ff32f/apple-mbp-15-header.jpg',
+    imgUrl: 'https://via.placeholder.com/150',
     stars: 5,
   },
   {
@@ -12,7 +12,7 @@ export default [
     details: '16 GB ram, 500 GB SSD',
     price: 450000,
     category: ['electronics', 'apple', 'macbook'],
-    imgUrl: '',
+    imgUrl: 'https://via.placeholder.com/150',
     stars: 4,
   },
   {
@@ -25,7 +25,7 @@ export default [
     Tamaño de la pantalla 15.6.`,
     price: 550000,
     category: ['electronics', 'asus', 'laptop'],
-    imgUrl: 'https://img-us1.asus.com/A/show/AW000706/2018/0724/AM0000002/201807AM240000002_15323946034711030058669.jpg!t500x500',
+    imgUrl: 'https://via.placeholder.com/150',
     stars: 5,
   },
   {
@@ -33,7 +33,7 @@ export default [
     details: '8 GB ram, 256 GB SSD',
     price: 750800,
     category: ['electronics', 'hp', 'laptop'],
-    imgUrl: 'https://media.aws.alkosto.com/media/catalog/product/cache/6/image/69ace863370f34bdf190e4e164b6e123/1/9/192545439077-01.jpg',
+    imgUrl: 'https://via.placeholder.com/150',
     stars: 3,
   },
   {
@@ -41,7 +41,7 @@ export default [
     details: '8 GB ram, 256 GB SSD',
     price: 1750000,
     category: ['electronics', 'apple', 'macbook'],
-    imgUrl: '',
+    imgUrl: 'https://via.placeholder.com/150',
     stars: 5,
   },
 ];
