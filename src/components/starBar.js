2ea84e055aa866ca9c98fca3baf7620d2ba22bfa
Thomas Reisenegger <treisenegger@github.com>
2019-05-13 22:52:25 -0400

    Implement star behavior

--- src/components/starBar.js
+++ src/components/starBar.js
@@ -5,29 +5,41 @@ import "./starBar.scss";
 
 export default class StarBar extends HTMLElement {
   get stars() {
-    return this.getAttribute('stars');
+    return +this.getAttribute('stars');
   }
 
   set stars(val) {
-    this.setAttribute('stars', val);
+    this.setAttribute('stars', +val);
+    this.firstChild.childNodes.forEach(child => {
+      child.setAttribute('src', +child.getAttribute('value') < this.stars ? `${star}` : `${starGrey}`);
+      child.className = +child.getAttribute('value') < this.stars ? "star" : "star-grey";
+    });
   }
+
   get total() {
-    return this.getAttribute('total');
+    return +this.getAttribute('total');
   }
 
   set total(val) {
-    this.setAttribute('total', val);
+    this.setAttribute('total', +val);
   }
 
   connectedCallback() {
     let content = '<div>';
     for (let i = 0; i < this.stars; i++) {
-      content += `<img src="${star}" class="star"/>`;
+      content += `<img src="${star}" value="${i}" class="star"/>`;
     }
     for (let i = 0; i < this.total - this.stars; i++) {
-      content += `<img src="${starGrey}" class="star-grey"/>`;
+      content += `<img src="${starGrey}" value="${this.stars+i}" class="star-grey"/>`;
     }
     content += '<div/>';
     this.innerHTML = content;
+    
+    this.firstChild.childNodes.forEach(child => {
+      child.addEventListener('click', event => {
+        const clicked = event.target;
+        this.stars = +clicked.getAttribute("value")+1;
+      });
+    });
   }
 }
