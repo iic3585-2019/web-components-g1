8d42debc20256f8aaac4293f0d9f9c4524070ee2
Florencia Valladares <fvalladares1@uc.cl>
2019-05-15 11:11:51 -0400

    change productr container filter

--- src/components/products-container.js
+++ src/components/products-container.js
@@ -18,9 +18,16 @@ export default class ProductsContainer extends HTMLElement {
   filterProductsByName(text) {
     const filtered = allProducts.filter(
         (product) => product.name.toLowerCase().includes(text.toLowerCase())
-    );
+    ).map((product) => product.name);
     const containerHTML = this.firstChild.childNodes[1];
-    containerHTML.innerHTML = mapProductsToHTML(filtered);
+    containerHTML.childNodes.forEach((child) => {
+      if (filtered.includes(child.name)) {
+        child.style.display = '';
+      } else {
+        child.style.display = 'none';
+      }
+    });
+    // containerHTML.innerHTML = mapProductsToHTML(filtered);
   }
 
   connectedCallback() {
